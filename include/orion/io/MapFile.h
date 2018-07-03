52d97caa533fdf823c1665ada69b74b5ee56d7ba
Tomas Palazuelos <tomas.plzls@gmail.com>
2018-07-03 10:07:52 -0400

    Create MapFile class.

---
+++ include/orion/io/MapFile.h
@@ -0,0 +1,104 @@
+//
+// MapFile.h
+//
+// Copyright (c) 2013-2018 Tomas Palazuelos
+//
+// Distributed under the MIT Software License. (See accompanying file LICENSE.md)
+//
+#ifndef ORION_IO_MAPFILE_H
+#define ORION_IO_MAPFILE_H
+
+#include <orion/Orion-Stddefs.h>
+
+#include <memory>
+#include <system_error>
+
+namespace orion
+{
+namespace io
+{
+struct MapFileImpl;
+
+enum class MapFlags : uint8_t
+{
+   ReadOnly  = 0x02, // ReadOnly maps the memory read-only.
+   ReadWrite = 0x04, // ReadWrite maps the memory as read-write. Writes to the Map object
+                     // will update the underlying file.
+   Copy = 0x08,      // Copy maps the memory as copy-on-write. Writes to the Map object will affect
+                     // memory, but the underlying file will remain unchanged.
+   Execute = 0x10    // If Execute is set, the mapped memory is marked as executable.
+};
+
+///
+/// MapFile allows mapping files into memory. It tries to provide a simple, reasonably
+/// portable interface, but doesn't abstract away every little
+/// platform detail.
+///
+class API_EXPORT MapFile
+{
+public:
+   MapFile();
+   ~MapFile();
+
+   MapFile(std::unique_ptr<MapFileImpl>&& priv);
+
+   // Move
+   MapFile(MapFile&& rhs) noexcept;
+   MapFile& operator=(MapFile&& rhs) noexcept;
+
+   // No copy
+   MapFile(const MapFile&) = delete;
+   MapFile& operator=(const MapFile&) = delete;
+
+   /// File size
+   std::size_t size() const;
+
+   /// Size of actually mapped bytes
+   std::size_t mapped_size() const;
+
+   uint8_t* data();
+
+   const uint8_t* data() const;
+
+   uintptr_t file_handle() const;
+
+   /// Lock keeps the mapped region in physical memory, ensuring that it will not be
+   /// swapped out.
+   std::error_code lock();
+
+   /// Unlock reverses the effect of lock, allowing the mapped region to potentially
+   /// be swapped out.
+   std::error_code unlock();
+
+   /// Flush synchronizes the mapping's contents to the file's contents on disk.
+   std::error_code flush();
+
+   /// Unmap deletes the memory mapped region, flushes any remaining changes.
+   std::error_code unmap();
+
+private:
+   const MapFileImpl* impl() const { return _impl.get(); }
+   MapFileImpl* impl() { return _impl.get(); }
+
+   std::unique_ptr<MapFileImpl> _impl;
+};
+
+API_EXPORT MapFile make_mapfile(const std::string& file_name,
+                                MapFlags flags,
+                                std::size_t bytes_to_map = 0, // 0 = Whole file
+                                uint64_t offset          = 0);
+
+API_EXPORT MapFile make_mapfile(const std::wstring& file_name,
+                                MapFlags flags,
+                                std::size_t bytes_to_map = 0, // 0 = Whole file
+                                uint64_t offset          = 0);
+
+API_EXPORT MapFile make_mapfile(uintptr_t file_handle,
+                                MapFlags flags,
+                                std::size_t bytes_to_map = 0, // 0 = Whole file
+                                uint64_t offset          = 0);
+
+} // namespace io
+} // namespace orion
+
+#endif // ORION_IO_MAPFILE_H
---
+++ lib/io/MapFile-win32.cpp
@@ -0,0 +1,254 @@
+//
+// MapFile-win32.cpp
+//
+// Copyright (c) 2013-2017 Tomas Palazuelos
+//
+// Distributed under the MIT Software License. (See accompanying file LICENSE.md)
+//
+#include <orion/io/MapFile.h>
+
+#include <orion/ErrorMacros.h>
+#include <orion/Exception.h>
+#include <orion/SourceLocation.h>
+#include <orion/Throw.h>
+
+#include <host/win32/Error.h>
+#include <host/win32/String.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+namespace orion
+{
+namespace io
+{
+
+struct MapFileImpl
+{
+   HANDLE file_handle      = INVALID_HANDLE_VALUE;
+   HANDLE file_mapping     = INVALID_HANDLE_VALUE;
+   uint8_t* view_address   = nullptr; // Base byte* of mapped file
+   std::size_t file_size   = 0;
+   std::size_t mapped_size = 0;
+};
+
+static std::unique_ptr<MapFileImpl> make_impl(HANDLE file_handle,
+                                              MapFlags flags,
+                                              std::size_t bytes_to_map,
+                                              uint64_t offset);
+
+//--------------------------------------------------------------------------------------------------
+
+MapFile::MapFile()
+   : _impl(std::make_unique<MapFileImpl>())
+{
+}
+
+MapFile::MapFile(MapFile&& rhs) noexcept
+   : _impl(std::move(rhs._impl))
+{
+}
+
+MapFile::MapFile(std::unique_ptr<MapFileImpl>&& priv)
+   : _impl(std::move(priv))
+{
+}
+
+MapFile::~MapFile() {}
+
+MapFile& MapFile::operator=(MapFile&& rhs) noexcept
+{
+   _impl = std::move(rhs._impl);
+
+   return *this;
+}
+
+std::size_t MapFile::size() const
+{
+   return impl()->file_size;
+}
+
+std::size_t MapFile::mapped_size() const
+{
+   return impl()->mapped_size;
+}
+
+uint8_t* MapFile::data()
+{
+   return impl()->view_address;
+}
+
+const uint8_t* MapFile::data() const
+{
+   return impl()->view_address;
+}
+
+uintptr_t MapFile::file_handle() const
+{
+   return reinterpret_cast<uintptr_t>(impl()->file_handle);
+}
+
+std::error_code MapFile::lock()
+{
+   if (not VirtualLock(impl()->view_address, impl()->mapped_size))
+      return win32::make_error_code(GetLastError());
+
+   return std::error_code();
+}
+
+std::error_code MapFile::unlock()
+{
+   if (not VirtualUnlock(impl()->view_address, impl()->mapped_size))
+      return win32::make_error_code(GetLastError());
+
+   return std::error_code();
+}
+
+std::error_code MapFile::flush()
+{
+   if (not FlushViewOfFile(impl()->view_address, impl()->mapped_size))
+      return win32::make_error_code(GetLastError());
+
+   return std::error_code();
+}
+
+std::error_code MapFile::unmap()
+{
+   if (not UnmapViewOfFile(impl()->view_address))
+      return win32::make_error_code(GetLastError());
+
+   CloseHandle(impl()->file_mapping);
+
+   if (impl()->file_handle != INVALID_HANDLE_VALUE)
+      CloseHandle(impl()->file_handle);
+
+   return std::error_code();
+}
+
+//--------------------------------------------------------------------------------------------------
+
+MapFile make_mapfile(const std::string& file_name,
+                     MapFlags flags,
+                     std::size_t bytes_to_map,
+                     uint64_t offset)
+{
+   std::wstring fname = win32::utf8_to_wstring(file_name);
+
+   return make_mapfile(fname, flags, bytes_to_map, offset);
+}
+
+MapFile make_mapfile(const std::wstring& file_name,
+                     MapFlags flags,
+                     std::size_t bytes_to_map,
+                     uint64_t offset)
+{
+   HANDLE file_handle = CreateFileW(file_name.c_str(),
+                                    GENERIC_READ,
+                                    FILE_SHARE_READ,
+                                    nullptr,
+                                    OPEN_EXISTING,
+                                    FILE_ATTRIBUTE_NORMAL,
+                                    0);
+
+   if (file_handle == INVALID_HANDLE_VALUE)
+   {
+      // Error
+      return {};
+   }
+
+   std::unique_ptr<MapFileImpl> impl = make_impl(file_handle, flags, bytes_to_map, offset);
+
+   impl->file_handle = file_handle;
+
+   return MapFile{std::move(impl)};
+}
+
+MapFile make_mapfile(uintptr_t file_handle,
+                     MapFlags flags,
+                     std::size_t bytes_to_map,
+                     uint64_t offset)
+{
+   std::unique_ptr<MapFileImpl> impl = make_impl((HANDLE)file_handle, flags, bytes_to_map, offset);
+
+   return MapFile{std::move(impl)};
+}
+
+//--------------------------------------------------------------------------------------------------
+
+std::unique_ptr<MapFileImpl> make_impl(HANDLE file_handle,
+                                       MapFlags flags,
+                                       std::size_t bytes_to_map,
+                                       uint64_t offset)
+{
+   DWORD protect = PAGE_READONLY;
+   DWORD access  = FILE_MAP_READ;
+
+   switch (flags)
+   {
+      case MapFlags::ReadOnly:
+         break;
+      case MapFlags::ReadWrite:
+         protect = PAGE_READWRITE;
+         access  = FILE_MAP_WRITE;
+         break;
+      case MapFlags::Copy:
+         protect = PAGE_WRITECOPY;
+         access  = FILE_MAP_COPY;
+         break;
+      case MapFlags::Execute:
+         break;
+   }
+
+   // Get file size
+   LARGE_INTEGER win32_file_size;
+   if (not GetFileSizeEx(file_handle, &win32_file_size))
+   {
+      // Error
+      throw std::system_error(win32::make_error_code(GetLastError()));
+      return {};
+   }
+   std::size_t file_size = static_cast<std::size_t>(win32_file_size.QuadPart);
+
+   if (offset > file_size)
+   {
+      // Error
+      throw std::system_error(win32::make_error_code(GetLastError()));
+      return {};
+   }
+
+   HANDLE file_mapping = CreateFileMapping(file_handle, nullptr, protect, 0, 0, nullptr);
+   if (not file_mapping)
+   {
+      // Error
+      throw std::system_error(win32::make_error_code(GetLastError()));
+      return {};
+   }
+
+   if ((offset + bytes_to_map) > file_size)
+      bytes_to_map = std::size_t(file_size - offset);
+
+   DWORD offset_low  = DWORD(offset & 0xFFFFFFFF);
+   DWORD offset_high = DWORD(offset >> 32);
+
+   // Get mapped view address
+   uint8_t* view_address =
+      (uint8_t*)MapViewOfFile(file_mapping, access, offset_high, offset_low, bytes_to_map);
+   if (view_address == nullptr)
+   {
+      CloseHandle(file_mapping);
+      throw std::system_error(win32::make_error_code(GetLastError()));
+      return {};
+   }
+
+   // mapped_size = (bytes_to_map == 0) ? file_size : bytes_to_map;
+
+   return std::make_unique<MapFileImpl>(
+      MapFileImpl{file_handle,
+                  file_mapping,
+                  view_address,
+                  file_size,
+                  ((bytes_to_map == 0) ? file_size : bytes_to_map)});
+}
+
+} // namespace io
+} // namespace orion
